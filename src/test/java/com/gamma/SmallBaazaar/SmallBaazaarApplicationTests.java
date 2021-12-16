e17286867324f7a2333d22a92fea18925fb227b4
Ajinkya Suryawanshi <ajinkya.s@DM1370.datametica.com>
2021-12-16 16:45:54 +0530

    Initial Commit

---
+++ src/test/java/com/gamma/SmallBaazaar/SmallBaazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamma.SmallBaazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallBaazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
